cfd54d772a7803f89abd5f98368eb135942b88fd
natans <natans@wix.com>
2017-11-28 11:31:50 +0200

    fix indentation

--- tomono.sh
+++ tomono.sh
@@ -12,19 +12,19 @@
 
 ${DEBUGSH:+set -x}
 if [[ "$BASH_SOURCE" == "$0" ]]; then
-	is_script=true
-	set -eu -o pipefail
+    is_script=true
+    set -eu -o pipefail
 else
-	is_script=false
+    is_script=false
 fi
 
 
 function read_repositories {
-	sed -e 's/#.*//' | grep .
+    sed -e 's/#.*//' | grep .
 }
 
 function remote-branches {
-	git branch -r | grep "^  $1/" | sed -e "s_$1/__"
+    git branch -r | grep "^  $1/" | sed -e "s_$1/__"
 }
 
 # Create a monorepository in a directory "core". Read repositories from STDIN:
@@ -33,90 +33,90 @@ function remote-branches {
 # 1. The (git cloneable) location of the repository
 # 2. The name of the target directory in the core repository
 function create-mono {
-	echo "mono-repo ssh url: " $1
-	url=$1
+    echo "mono-repo ssh url: " $1
+    url=$1
     echo "mono-repo name: " $2
     MONOREPO_NAME=$2
 
-	# Pretty risky, check double-check!
-	if [[ "${3:-}" == "--continue" ]]; then
-		if [[ ! -d "$MONOREPO_NAME" ]]; then
-			echo "--continue specified, but nothing to resume" >&2
-			exit 1
-		fi
-		pushd "$MONOREPO_NAME"
-	else
-		if [[ -d "$MONOREPO_NAME" ]]; then
-			echo "Target repository directory $MONOREPO_NAME already exists." >&2
-			return 1
-		fi
-		mkdir "$MONOREPO_NAME"
-		pushd "$MONOREPO_NAME"
-		git init
-		git remote add origin $url
-	fi
-	read_repositories | while read repo name; do
-		if [[ -z "$name" ]]; then
-			echo "pass REPOSITORY NAME pairs on stdin" >&2
-			return 1
-		fi
-		echo "Merging in $repo.." >&2
-		git remote add "$name" "$repo"
-		git fetch -qa "$name"
+    # Pretty risky, check double-check!
+    if [[ "${3:-}" == "--continue" ]]; then
+        if [[ ! -d "$MONOREPO_NAME" ]]; then
+            echo "--continue specified, but nothing to resume" >&2
+            exit 1
+        fi
+        pushd "$MONOREPO_NAME"
+    else
+        if [[ -d "$MONOREPO_NAME" ]]; then
+            echo "Target repository directory $MONOREPO_NAME already exists." >&2
+            return 1
+        fi
+        mkdir "$MONOREPO_NAME"
+        pushd "$MONOREPO_NAME"
+        git init
+        git remote add origin $url
+    fi
+    read_repositories | while read repo name; do
+        if [[ -z "$name" ]]; then
+            echo "pass REPOSITORY NAME pairs on stdin" >&2
+            return 1
+        fi
+        echo "Merging in $repo.." >&2
+        git remote add "$name" "$repo"
+        git fetch -qa "$name"
 
-		# Merge every branch from the sub repo into the mono repo, into a
-		# branch of the same name (create one if it doesn't exist).
-		remote-branches "$name" | while read branch; do
-			if git rev-parse -q --verify "$branch"; then
-				# Branch already exists, just check it out (and clean up the working dir)
-				git checkout -q "$branch"
-				git checkout -q -- .
-				git clean -f -d
-			else
-				# Create a fresh branch with an empty root commit"
-				git checkout -q --orphan "$branch"
-				# The ignore unmatch is necessary when this was a fresh repo
-				git rm -rfq --ignore-unmatch .
-				git commit -q --allow-empty -m "Root commit for $branch branch"
-			fi
-			####
-			# In order to preserve history per file, we move the files before merging
-			# We do this in a new local branch, without pushing the move to original repo
-			temp_branch="$name-$branch"
-	        git checkout -b "$temp_branch" "$name"/"$branch"
-	        mkdir "$name"
-	        find * -maxdepth 0 -not -path .git -not -path "$name" -exec git mv {} "$name" \;
-	        git commit -q -m "Moving into subdir $name to prepare for consolidation"
-	        git checkout -q "$branch"
-	        git merge -q "$temp_branch" --allow-unrelated-histories
-	        git branch -q -D "$temp_branch"
-	 		####
-		done
+        # Merge every branch from the sub repo into the mono repo, into a
+        # branch of the same name (create one if it doesn't exist).
+        remote-branches "$name" | while read branch; do
+            if git rev-parse -q --verify "$branch"; then
+                # Branch already exists, just check it out (and clean up the working dir)
+                git checkout -q "$branch"
+                git checkout -q -- .
+                git clean -f -d
+            else
+                # Create a fresh branch with an empty root commit"
+                git checkout -q --orphan "$branch"
+                # The ignore unmatch is necessary when this was a fresh repo
+                git rm -rfq --ignore-unmatch .
+                git commit -q --allow-empty -m "Root commit for $branch branch"
+            fi
+            ####
+            # In order to preserve history per file, we move the files before merging
+            # We do this in a new local branch, without pushing the move to original repo
+            temp_branch="$name-$branch"
+            git checkout -b "$temp_branch" "$name"/"$branch"
+            mkdir "$name"
+            find * -maxdepth 0 -not -path .git -not -path "$name" -exec git mv {} "$name" \;
+            git commit -q -m "Moving into subdir $name to prepare for consolidation"
+            git checkout -q "$branch"
+            git merge -q "$temp_branch" --allow-unrelated-histories
+            git branch -q -D "$temp_branch"
+            ####
+        done
                 
         for tag in `git ls-remote --tags $name | cut -f2 | grep -v "\^{}$" | sed -E 's/.*(RC;.*)/\1/g'`; do
-		  if [[ $tag =~ (.*)RC\;\.\;(.*) ]]; then
-		    fixed_tag=`echo $tag | sed -E "s/RC;.;(.*)/RC;$name;\1/" `
-		    echo $tag '-->' $fixed_tag
-		    git tag $fixed_tag $tag
-		    git tag -d $tag
-		  elif [[ $tag =~ (.*)RC\;(.*)\;(.*) ]]; then
-		    fixed_tag=`echo $tag | sed -E "s/RC;(.*);(.*)/RC;$name\/\1;\2/" `
-		    echo $tag '-->' $fixed_tag
-		    git tag $fixed_tag $tag
-		    git tag -d $tag
-		  fi;
-		done
+          if [[ $tag =~ (.*)RC\;\.\;(.*) ]]; then
+            fixed_tag=`echo $tag | sed -E "s/RC;.;(.*)/RC;$name;\1/" `
+            echo $tag '-->' $fixed_tag
+            git tag $fixed_tag $tag
+            git tag -d $tag
+          elif [[ $tag =~ (.*)RC\;(.*)\;(.*) ]]; then
+            fixed_tag=`echo $tag | sed -E "s/RC;(.*);(.*)/RC;$name\/\1;\2/" `
+            echo $tag '-->' $fixed_tag
+            git tag $fixed_tag $tag
+            git tag -d $tag
+          fi;
+        done
 
         echo 'finished changing tags (not pushed yet)'
  
-	done
-	
+    done
+    
     git checkout -q master
-	git checkout -q .
-	git push --all origin
-	git push --tags
+    git checkout -q .
+    git push --all origin
+    git push --tags
 }
 
 if [[ "$is_script" == "true" ]]; then
-	create-mono $@
+    create-mono $@
 fi
