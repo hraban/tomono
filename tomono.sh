0ac8ae9aaea676a4c68beaae039888bc705d914e
Douglas Reith <douglas@reith.com.au>
2018-03-21 13:15:07 +1100

    Replace tabs with 4 spaces

--- tomono.sh
+++ tomono.sh
@@ -12,10 +12,10 @@
 
 ${DEBUGSH:+set -x}
 if [[ "$BASH_SOURCE" == "$0" ]]; then
-	is_script=true
-	set -eu -o pipefail
+    is_script=true
+    set -eu -o pipefail
 else
-	is_script=false
+    is_script=false
 fi
 
 # Default name of the mono repository (override with envvar)
@@ -38,29 +38,29 @@ popd () {
 }
 
 function read_repositories {
-	sed -e 's/#.*//' | grep .
+    sed -e 's/#.*//' | grep .
 }
 
 # Simply list all files, recursively. No directories.
 function ls-files-recursive {
-	find . -type f | sed -e 's!..!!'
+    find . -type f | sed -e 's!..!!'
 }
 
 # List all branches for a given remote
 function remote-branches {
-	# With GNU find, this could have been:
-	#
-	#   find "$dir/.git/yada/yada" -type f -printf '%P\n'
-	#
-	# but it's not a real shell script if it's not compatible with a 14th
-	# century OS from planet zorploid borploid.
+    # With GNU find, this could have been:
+    #
+    #   find "$dir/.git/yada/yada" -type f -printf '%P\n'
+    #
+    # but it's not a real shell script if it's not compatible with a 14th
+    # century OS from planet zorploid borploid.
 
-	# Get into that git plumbing.  Cleanest way to list all branches without
-	# text editing rigmarole (hard to find a safe escape character, as we've
-	# noticed. People will put anything in branch names).
-	pushd "$monorepo_dir/.git/refs/remotes/$1/"
-	ls-files-recursive
-	popd
+    # Get into that git plumbing.  Cleanest way to list all branches without
+    # text editing rigmarole (hard to find a safe escape character, as we've
+    # noticed. People will put anything in branch names).
+    pushd "$monorepo_dir/.git/refs/remotes/$1/"
+    ls-files-recursive
+    popd
 }
 
 # Create a monorepository in a directory "core". Read repositories from STDIN:
@@ -69,74 +69,75 @@ function remote-branches {
 # 1. The (git cloneable) location of the repository
 # 2. The name of the target directory in the core repository
 function create-mono {
-	# Pretty risky, check double-check!
-	if [[ "${1:-}" == "--continue" ]]; then
-		if [[ ! -d "$MONOREPO_NAME" ]]; then
-			echo "--continue specified, but nothing to resume" >&2
-			exit 1
-		fi
-		pushd "$MONOREPO_NAME"
-	else
-		if [[ -d "$MONOREPO_NAME" ]]; then
-			echo "Target repository directory $MONOREPO_NAME already exists." >&2
-			return 1
-		fi
-		mkdir "$MONOREPO_NAME"
-		pushd "$MONOREPO_NAME"
-		git init
-	fi
+    # Pretty risky, check double-check!
+    if [[ "${1:-}" == "--continue" ]]; then
+        if [[ ! -d "$MONOREPO_NAME" ]]; then
+            echo "--continue specified, but nothing to resume" >&2
+            exit 1
+        fi
+        pushd "$MONOREPO_NAME"
+    else
+        if [[ -d "$MONOREPO_NAME" ]]; then
+            echo "Target repository directory $MONOREPO_NAME already exists." >&2
+            return 1
+        fi
+        mkdir "$MONOREPO_NAME"
+        pushd "$MONOREPO_NAME"
+        git init
+    fi
 
-	# This directory will contain all final tag refs (namespaced)
-	mkdir -p .git/refs/namespaced-tags
+    # This directory will contain all final tag refs (namespaced)
+    mkdir -p .git/refs/namespaced-tags
 
-	read_repositories | while read repo name; do
+    read_repositories | while read repo name; do
 
-		if [[ -z "$name" ]]; then
-			echo "pass REPOSITORY NAME pairs on stdin" >&2
-			return 1
-		elif [[ "$name" = */* ]]; then
-			echo "Forward slash '/' not supported in repo names: $name" >&2
-			return 1
-		fi
-		echo "Merging in $repo.." >&2
-		git remote add "$name" "$repo"
-		echo "Fetching $name.." >&2 
-		git fetch -q "$name"
+        if [[ -z "$name" ]]; then
+            echo "pass REPOSITORY NAME pairs on stdin" >&2
+            return 1
+        elif [[ "$name" = */* ]]; then
+            echo "Forward slash '/' not supported in repo names: $name" >&2
+            return 1
+        fi
 
-		# Now we've got all tags in .git/refs/tags: put them away for a sec
-		if [[ -n "$(ls .git/refs/tags)" ]]; then
-			mv .git/refs/tags ".git/refs/namespaced-tags/$name"
-		fi
+        echo "Merging in $repo.." >&2
+        git remote add "$name" "$repo"
+        echo "Fetching $name.." >&2 
+        git fetch -q "$name"
 
-		# Merge every branch from the sub repo into the mono repo, into a
-		# branch of the same name (create one if it doesn't exist).
-		remote-branches "$name" | while read branch; do
-			if git rev-parse -q --verify "$branch"; then
-				# Branch already exists, just check it out (and clean up the working dir)
-				git checkout -q "$branch"
-				git checkout -q -- .
-				git clean -f -d
-			else
-				# Create a fresh branch with an empty root commit"
-				git checkout -q --orphan "$branch"
-				# The ignore unmatch is necessary when this was a fresh repo
-				git rm -rfq --ignore-unmatch .
-				git commit -q --allow-empty -m "Root commit for $branch branch"
-			fi
-			git merge -q --no-commit -s ours "$name/$branch" --allow-unrelated-histories
-			git read-tree --prefix="$name/" "$name/$branch"
-			git commit -q --no-verify --allow-empty -m "Merging $name to $branch"
-		done
-	done
+        # Now we've got all tags in .git/refs/tags: put them away for a sec
+        if [[ -n "$(ls .git/refs/tags)" ]]; then
+            mv .git/refs/tags ".git/refs/namespaced-tags/$name"
+        fi
 
-	# Restore all namespaced tags
-	rm -rf .git/refs/tags
-	mv .git/refs/namespaced-tags .git/refs/tags
+        # Merge every branch from the sub repo into the mono repo, into a
+        # branch of the same name (create one if it doesn't exist).
+        remote-branches "$name" | while read branch; do
+            if git rev-parse -q --verify "$branch"; then
+                # Branch already exists, just check it out (and clean up the working dir)
+                git checkout -q "$branch"
+                git checkout -q -- .
+                git clean -f -d
+            else
+                # Create a fresh branch with an empty root commit"
+                git checkout -q --orphan "$branch"
+                # The ignore unmatch is necessary when this was a fresh repo
+                git rm -rfq --ignore-unmatch .
+                git commit -q --allow-empty -m "Root commit for $branch branch"
+            fi
+            git merge -q --no-commit -s ours "$name/$branch" --allow-unrelated-histories
+            git read-tree --prefix="$name/" "$name/$branch"
+            git commit -q --no-verify --allow-empty -m "Merging $name to $branch"
+        done
+    done
 
-	git checkout -q master
-	git checkout -q .
+    # Restore all namespaced tags
+    rm -rf .git/refs/tags
+    mv .git/refs/namespaced-tags .git/refs/tags
+
+    git checkout -q master
+    git checkout -q .
 }
 
 if [[ "$is_script" == "true" ]]; then
-	create-mono "${1:-}"
+    create-mono "${1:-}"
 fi
