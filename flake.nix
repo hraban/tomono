7e7c67b51064f5d69dbb6052c5efab4e3e44e341
Hraban Luyat <hraban@0brg.net>
2024-06-04 12:25:54 -0400

    build: pin to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "Migrate multiple repositories into a single monorepo";
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = {
